e96620001941e1a3f74644c0785ced78215332b0
ai-24 <ai.morikawa2410sj@gmail.com>
2021-07-07 12:27:37 +0900

    complete rubocop

--- memo_app.rb
+++ memo_app.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
-require "json"
+require 'json'
 require 'securerandom'
 
 def files
-file_name = Dir.glob("*.json")
-file_name.map do |f|
-  File.open("#{f}",'r') do |file|
-    JSON.load(file)
+  file_name = Dir.glob('*.json')
+  file_name.map do |f|
+    File.open(f.to_s, 'r') do |file|
+      JSON.load(file)
+    end
   end
-end
 end
 
 get '/' do
   @tag = 'ホーム|メモアプリ'
-  @files = files.sort { |a,b| b["time"] <=> a["time"] }
+  @files = files.sort { |a, b| b['time'] <=> a['time'] }
   @size = @files.size
   erb :index
 end
@@ -29,12 +31,12 @@ post '/memos' do
   @title = params[:title]
   @content = params[:content]
   @file_name = SecureRandom.uuid
-  File.open("#{@file_name}.json",'w') do |file|
+  File.open("#{@file_name}.json", 'w') do |file|
     @time = File.ctime("#{@file_name}.json")
     hash = { id: @file_name, title: @title, content: @content, time: @time }
-    JSON.dump(hash,file)
+    JSON.dump(hash, file)
   end
-  redirect to("/memos")
+  redirect to('/memos')
   erb :detail
 end
 
@@ -46,14 +48,14 @@ end
 get '/memos/:id' do
   @tag = '詳細|メモアプリ'
   @id = params[:id]
-  @file_detail = files.find { |x| x["id"].include?(@id)}
+  @file_detail = files.find { |x| x['id'].include?(@id) }
   erb :detail
 end
 
 get '/memos/:id/edit' do
   @tag = '編集|メモアプリ'
   @id = params[:id]
-  @file_detail = files.find { |x| x["id"].include?(@id)}
+  @file_detail = files.find { |x| x['id'].include?(@id) }
   if @file_detail
     erb :edit_memo
   else
@@ -80,10 +82,10 @@ patch '/memos/:id' do
   @id = params[:id]
   @title = params[:edit_title]
   @content = params[:edit_content]
-  File.open("#{@id}.json",'w') do |file|
+  File.open("#{@id}.json", 'w') do |file|
     @time = File.ctime("#{@id}.json")
     hash = { id: @id, title: @title, content: @content, time: @time }
-    JSON.dump(hash,file)
+    JSON.dump(hash, file)
   end
   redirect to("memos/#{@id}/save")
   erb :save

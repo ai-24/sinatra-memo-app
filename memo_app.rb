8670d090743311dce5e18ac29ec67b98d88a3e9c
ai-24 <ai.morikawa2410sj@gmail.com>
2021-07-18 12:02:14 +0900

    fixed

--- memo_app.rb
+++ memo_app.rb
@@ -9,8 +9,7 @@ def files
   file_name = Dir.glob('*.json')
   file_name.map do |f|
     File.open(f, 'r') do |file|
-      file_content = file.read
-      JSON.parse(file_content)
+      JSON.parse(file.read)
     end
   end
 end
@@ -25,42 +24,32 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  @tag = '保存しました|メモアプリ'
-  @title = params[:title]
-  @content = params[:content]
-  @file_name = SecureRandom.uuid
-  File.open("#{@file_name}.json", 'w') do |file|
-    @time = File.ctime("#{@file_name}.json")
-    hash = { id: @file_name, title: @title, content: @content, time: @time }
+  title = params[:title]
+  content = params[:content]
+  file_name = SecureRandom.uuid
+  time = Time.now
+  File.open("#{file_name}.json", 'w') do |file|
+    hash = { id: file_name, title: title, content: content, time: time }
     JSON.dump(hash, file)
   end
   redirect to('/memos?complete=1')
-  erb :detail
 end
 
 get '/memos' do
   @tag = 'ホーム|メモアプリ'
   @files = files.sort_by { |f| [f['time']] }.reverse
   @size = @files.size
-  if params['complete']
-    erb :save
-  else
-    erb :index
-  end
+  erb :index
 end
 
 get '/memos/:id' do
   @tag = '詳細|メモアプリ'
   @id = params[:id]
-  @file_detail = files.find { |x| x['id'].include?(@id) }
+  @file_detail = files.find { |x| x['id'] == @id }
   if @file_detail
     erb :detail
   else
-    not_found do
-      status 404
-      @tag = '404 ページが存在しません'
-      '404 ページが存在しません'
-    end
+    redirect to('not_found')
   end
 end
 
@@ -76,36 +65,21 @@ get '/memos/:id/edit' do
 end
 
 delete '/memos/:id' do
-  @tag = '削除|メモアプリ'
-  @id = params[:id]
-  File.delete("#{@id}.json")
-  redirect to("/memos/#{@id}/delete")
-  erb :delete
-end
-
-get '/memos/:id/delete' do
-  @tag = '削除|メモアプリ'
-  @id = params[:id]
-  erb :delete
+  id = params[:id]
+  File.delete("#{id}.json")
+  redirect to('/memos?delete=1')
 end
 
 patch '/memos/:id' do
-  @tag = '編集|メモアプリ'
-  @id = params[:id]
-  @title = params[:edit_title]
-  @content = params[:edit_content]
-  File.open("#{@id}.json", 'w') do |file|
-    @time = File.ctime("#{@id}.json")
-    hash = { id: @id, title: @title, content: @content, time: @time }
+  id = params[:id]
+  title = params[:edit_title]
+  content = params[:edit_content]
+  File.open("#{id}.json", 'w') do |file|
+    time = Time.now
+    hash = { id: id, title: title, content: content, time: time }
     JSON.dump(hash, file)
   end
-  redirect to("memos/#{@id}/save")
-end
-
-get '/memos/:id/save' do
-  @tag = '編集|メモアプリ'
-  @id = params[:id]
-  erb :edit_save
+  redirect to('memos?save=1')
 end
 
 helpers do
